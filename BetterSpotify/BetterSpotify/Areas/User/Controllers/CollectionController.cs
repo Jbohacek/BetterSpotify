bdf5b17151977e3de9b465373cbd46e0c069fc47
Jbohacek <jbohacek@email.cz>
2023-02-23 21:43:29 +0100

    Ability to play music
    -Collections
    -Music play

---
+++ BetterSpotify/BetterSpotify/Areas/User/Controllers/CollectionController.cs
@@ -0,0 +1,27 @@
+﻿using BetterSpotify.DataAccess.Repository._IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BetterSpotifyWeb.Areas.User.Controllers
+{
+    [Area("User")]
+    public class CollectionController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CollectionController(IUnitOfWork work)
+        {
+            _unitOfWork = work;
+        }
+
+        public IActionResult Index()
+        {
+            var listusers = _unitOfWork.Users.GetAll();
+            var listSongs = _unitOfWork.Songs.GetAll();
+            foreach (var VARIABLE in listSongs)
+            {
+                VARIABLE.User = listusers.FirstOrDefault(x => VARIABLE.IdUser == x.IdUser);
+            }
+            return View(listSongs);
+        }
+    }
+}
