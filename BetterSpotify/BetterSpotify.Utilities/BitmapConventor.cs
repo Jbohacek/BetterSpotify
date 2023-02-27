0cd1603d2a4bbcca7a0a6307df1f451b273d2e9a
Oliver Hruban <HrubanOliver@sssvt.cz>
2023-02-27 21:01:54 +0100

    Create BitmapConventor.cs
    Add imagePath to file class

---
+++ BetterSpotify/BetterSpotify.Utilities/BitmapConventor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BetterSpotify.Utilities
+{
+    public class BitmapConventor
+    {
+
+        public static Bitmap MakeBitmap(string path)
+        {
+            try
+            {
+                Bitmap bm = new Bitmap(path);
+                return bm;
+            }
+            catch
+            {
+                Console.WriteLine("Wrong path of file");
+            }
+            return null;
+        }
+
+    }
+}
