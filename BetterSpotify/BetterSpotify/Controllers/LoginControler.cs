cbd7b8b606d83f53fb7cd46c5112a7b059d71a90
Oliver Hruban <HrubanOliver@sssvt.cz>
2023-02-28 21:32:04 +0100

    Create LoginControler.cs

---
+++ BetterSpotify/BetterSpotify/Controllers/LoginControler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BetterSpotify.Models.Database;
+using BetterSpotify.DataAccess.Data;
+
+namespace BetterSpotifyWeb.Controllers
+{
+    public class LoginControler : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Verify(User user)
+        {
+            var qery = from d in _dbContext.TbUsers
+                       where d.IdUserProfile == user.IdUserProfile && d.Password == user.Password
+                       select d;
+
+            if(qery.Count() > 0 )
+            {
+                return View("LogIn");
+            }
+            else
+            {
+                return View("Error");
+            }
+            return View();  
+        }
+    }
+}
