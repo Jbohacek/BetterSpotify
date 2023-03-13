e8194ffd059901a3ec5b64b8cb74b462f976ae06
Jbohacek <jbohacek@email.cz>
2023-03-13 18:00:03 +0100

    CategoryShow

---
+++ BetterSpotify/BetterSpotify/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,76 @@
+﻿using BetterSpotify.DataAccess.Repository._IRepository;
+using BetterSpotify.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BetterSpotifyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public CategoryController(IUnitOfWork db, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = db;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index(int? id)
+        {
+            return View();
+        }
+
+        public IActionResult Upsert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(BetterSpotify.Models.Database.User item, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwRooPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string filename = Guid.NewGuid().ToString() + "ProfilePic";
+                    var uploads = Path.Combine(wwwRooPath, @"images/ProfilePics");
+                    var extension = Path.GetExtension(file.FileName);
+
+                    using (var filestream = new FileStream(Path.Combine(uploads, filename + extension), FileMode.Create))
+                    {
+                        file.CopyTo(filestream);
+                    }
+
+                    item.ImageFile = @"\images\ProfilePics\" + filename + extension;
+
+                }
+
+                for (int i = 0; i != 4; i++)
+                {
+                    item.AddId += new Random().Next(0, 9);
+                }
+
+                item.DateOfRegistration = DateTime.Now;
+                item.Password = Encription.Encrypt(item.Password);
+
+                _unitOfWork.Users.Add(item);
+                _unitOfWork.Save();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        #region ApiTable
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var list = _unitOfWork.Category.GetAll();
+            return Json(new { data = list });
+        }
+
+        #endregion
+    }
+}
---
+++ BetterSpotify/BetterSpotify/wwwroot/js/TableView/Category.js
@@ -0,0 +1,25 @@
+﻿
+
+var dataTable
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable()
+{
+    dataTable = $('#tblData').DataTable(
+        {
+        "ajax": {
+            "url":"/Admin/Category/GetAll"
+        },
+            "columns": [
+                { "data": "idCategory", "width": "1%" },
+                { "data": "name", "width": "25%" },
+                { "data": "colorHex", "width": "15%" },
+                { "data": "imageFile", "width": "100%" },
+            ]
+
+        },
+    );
+}
