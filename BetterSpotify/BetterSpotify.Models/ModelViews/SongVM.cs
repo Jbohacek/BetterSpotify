1d56848a9a3c4d0ba1e4d72a8390c7384e6631f2
Jbohacek <jbohacek@email.cz>
2023-03-14 22:37:39 +0100

    add SongVM

---
+++ BetterSpotify/BetterSpotify.Models/ModelViews/SongVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BetterSpotify.Models.Database;
+
+namespace BetterSpotify.Models.ModelViews
+{
+    public class SongVM
+    {
+        private Song song { get; set; }
+    }
+}
