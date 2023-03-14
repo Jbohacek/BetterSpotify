71f1b8abca90346d36d3e042e2472cc36b9a3a19
Jbohacek <jbohacek@email.cz>
2023-03-14 20:31:21 +0100

    Added EDIT and Delete to Users + sweetalerts

--- BetterSpotify/BetterSpotify/Areas/Admin/Controllers/UserController.cs
+++ BetterSpotify/BetterSpotify/Areas/Admin/Controllers/UserController.cs
@@ -15,15 +15,24 @@ namespace BetterSpotifyWeb.Areas.Admin.Controllers
             _unitOfWork = db;
             _webHostEnvironment = webHostEnvironment;
         }
-        public IActionResult Index(int? id)
+        public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Upsert()
+
+        //get
+        public IActionResult Upsert(int? id)
         {
-            return View();
+            BetterSpotify.Models.Database.User item = new();
+
+            if (id == null || id == 0) return View(item);
+
+            var FoundItem = _unitOfWork.Users.GetFirstOrDefault(x => x.IdUser == id);
+
+            return View(FoundItem);
         }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -38,6 +47,15 @@ namespace BetterSpotifyWeb.Areas.Admin.Controllers
                     var uploads = Path.Combine(wwwRooPath, @"images/ProfilePics");
                     var extension = Path.GetExtension(file.FileName);
 
+                    if (item.ImageFile != null)
+                    {
+                        var oldImagePath = Path.Combine(wwwRooPath, item.ImageFile.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var filestream = new FileStream(Path.Combine(uploads, filename + extension), FileMode.Create))
                     {
                         file.CopyTo(filestream);
@@ -47,22 +65,56 @@ namespace BetterSpotifyWeb.Areas.Admin.Controllers
 
                 }
 
-                for (int i = 0; i != 4; i++)
+                if (item.IdUser == 0)
                 {
-                    item.AddId += new Random().Next(0, 9);
+                    for (int i = 0; i != 4; i++)
+                    {
+                        item.AddId += new Random().Next(0, 9);
+                    }
+
+                    item.DateOfRegistration = DateTime.Now;
+                    item.Password = Encription.Encrypt(item.Password);
+
+                    _unitOfWork.Users.Add(item);
+                    _unitOfWork.Save();
+                }
+                else
+                {
+                    _unitOfWork.Users.Update(item);
+                    _unitOfWork.Save();
                 }
 
-                item.DateOfRegistration = DateTime.Now;
-                item.Password = Encription.Encrypt(item.Password);
-
-                _unitOfWork.Users.Add(item);
-                _unitOfWork.Save();
+                
             }
 
             return RedirectToAction("Index");
         }
 
         #region ApiTable
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            if (id == null) return RedirectToAction("Index");
+
+            var item = _unitOfWork.Users.GetFirstOrDefault(x => x.IdUser == id);
+
+            if (item == null) return RedirectToAction("Index");
+
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+
+            var oldImage = Path.Combine(wwwRootPath, item.ImageFile.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImage))
+            {
+                System.IO.File.Delete(oldImage);
+            }
+
+            _unitOfWork.Users.Remove(item);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
 
         [HttpGet]
         public IActionResult GetAll()
--- BetterSpotify/BetterSpotify/wwwroot/js/TableView/user.js
+++ BetterSpotify/BetterSpotify/wwwroot/js/TableView/user.js
@@ -14,18 +14,65 @@ function loadDataTable()
             "url":"/Admin/User/GetAll"
         },
             "columns": [
-            { "data": "idUser", "width": "1%" },
-            { "data": "nickName", "width": "25%" },
-            { "data": "firstName", "width": "15%" },
-            { "data": "lastName", "width": "15%" },
-            { "data": "email", "width": "25%" },
-            { "data": "country", "width": "15%" },
-            { "data": "getDateOfBirth", "width": "10%" },
-            { "data": "getDateOfRegistration", "width": "10%" },
-            { "data": "activeAccount", "width": "15%" },
-            { "data": "verified", "width": "15%" },
-            ]
+                { "data": "idUser", "width": "1%" },
+                {
+                    "data": "idUser",
+                    "render": function (data) {
+                        return `<div class="btn-group" role="group">
+                                <a class="btn btn-secondary mx-1" href="/Admin/User/Upsert?id=${data}"><i class="bi bi-pencil-square"></i></a>
+                                <a class="btn btn-danger mx-1" onClick=Delete('/Admin/User/Delete/${data}')><i class="bi bi-eraser"></i></a>
+                                </div>`
+                    },
+                    "width": "15%"
+                },
+                { "data": "nickName", "width": "25%" },
+                { "data": "firstName", "width": "15%" },
+                { "data": "lastName", "width": "15%" },
+                { "data": "email", "width": "25%" },
+                { "data": "country", "width": "15%" },
+                { "data": "getDateOfBirth", "width": "10%" },
+                { "data": "getDateOfRegistration", "width": "10%" },
+                { "data": "activeAccount", "width": "15%" },
+                { "data": "verified", "width": "15%" },
+                ]
 
-        },
+        }
     );
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            console.log('Uspech');
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    console.log("neco");
+                    if (data.success) {
+                        $('#tblData').DataTable().ajax.reload();
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success');
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Something went wrong!'
+                        })
+                    }
+                }
+            })
+
+        }
+    })
 }
