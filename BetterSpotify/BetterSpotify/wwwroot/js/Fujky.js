f64c93d2e2b824cfe84edae3add0ac88e50004fe
bohacekjakub <bohacekjakub@sssvt.cz>
2023-03-27 12:28:25 +0200

    fetch javascript

--- BetterSpotify/BetterSpotify/wwwroot/js/Fujky.js
+++ BetterSpotify/BetterSpotify/wwwroot/js/Fujky.js
@@ -14,10 +14,30 @@ $.getJSON("/User/Collection/GetAll", async function (data) {
 
 });
 
-
-run();
-
-async function run() {
-    await sleep(1000);
+function funguj (data) {
+    console.log('click');
+    console.log(data);
     console.log(neco);
 }
+
+document.getElementById('but').addEventListener('click',funguj);;
+
+
+//Funkcni
+
+var info = null;
+
+fetch("/User/Collection/GetAll").then(response => response.json())
+    .then(data => info = data).then(infoZob);
+
+function infoZob() {
+    console.log('InfoZob');
+    console.log(info);
+}
+
+//run();
+
+//async function run() {
+//    await sleep(1000);
+//    console.log(neco);
+//}
