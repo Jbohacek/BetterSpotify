c5812f8e1bc92ff4f513ebec85a05945c102223d
Jbohacek <jbohacek@email.cz>
2023-05-28 22:58:12 +0200

    Added Knockout + Base

---
+++ BetterSpotify/BetterSpotify/wwwroot/js/DiscoverScreen.js
@@ -0,0 +1,33 @@
+﻿
+
+
+
+
+
+
+
+
+//Funkcni
+
+
+
+//fetch("/User/Collection/GetAll").then(response => response.json())
+//    .then(data => info = data).then(infoZob);
+
+
+
+
+
+function AppViewModel() {
+    var self = this;
+
+    
+    self.Load = function() {
+        console.log('happy');
+    }
+
+    self.Load();
+    
+}
+
+ko.applyBindings(new AppViewModel());
