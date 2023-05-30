1ac165065b7607a18fe28a2671ecef72a4200537
Jbohacek <jbohacek@email.cz>
2023-05-30 10:30:57 +0200

    Updates

---
+++ BetterSpotify/BetterSpotify/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BetterSpotifyWeb.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
