7eac245587e33213ef8added1ca6d2fbbee2b054
Oliver Hruban <oliver.hruban@plus4u.net>
2023-06-03 16:03:04 +0200

    commit

--- BetterSpotify/BetterSpotify/Areas/Api/Controllers/SongController.cs
+++ BetterSpotify/BetterSpotify/Areas/Api/Controllers/SongController.cs
@@ -1,14 +1,49 @@
 ﻿using BetterSpotifyWeb.Areas.Api.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using BetterSpotify.Models.Database;
+using BetterSpotify.Models.ModelViews;
+using BetterSpotify.DataAccess.Repository._IRepository;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
 
 namespace BetterSpotifyWeb.Areas.Api.Controllers
 {
     [Area("Api")]
     public class SongController : Controller, SongInterface
     {
-        public IActionResult GetAll(int page, int pageCount, bool order, string? searchText, string? orderBy, int[]? catId)
+        private  IUnitOfWork _unitOfWork;
+        //
+        public IActionResult GetAll(int page, int pageCount, bool order, string? searchText, int[]? catId)
         {
-            return Json("Ahoj");
+           List<Song> list = _unitOfWork.Songs.GetAll().ToList();
+            
+            foreach (Song song in list)
+            {
+                if (!catId.Contains(song.IdCategory))
+                {
+                    list.Remove(song);  
+                }
+                else if (song.Title.ToLower() != searchText.ToLower() && song.Title.Substring(0,searchText.Length).ToLower() != searchText.ToLower()) 
+                {
+                    list.Remove(song);
+                }
+            }
+
+            if (!order)
+            {
+                list.Reverse();
+            }
+
+            int selected = page * pageCount;
+            List<Song> returnList = new List<Song>();
+
+            for (int i = selected - pageCount + 1; i <= selected; i++)
+            {
+                returnList.Add(list[i]);
+            }
+           
+
+            return Json(returnList);
         }
     }
 }
--- BetterSpotify/BetterSpotify/Areas/Api/Interfaces/SongInterface.cs
+++ BetterSpotify/BetterSpotify/Areas/Api/Interfaces/SongInterface.cs
@@ -5,7 +5,7 @@ namespace BetterSpotifyWeb.Areas.Api.Interfaces
     public interface SongInterface
     {
         [HttpGet]
-        public IActionResult GetAll(int page, int pageCount, bool order, string? searchText, string? orderBy, int[]? catId);
+        public IActionResult GetAll(int page, int pageCount, bool order, string? searchText, int[]? catId);
 
         
 
